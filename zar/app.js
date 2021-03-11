5a4661b85475480b87975f7843a8fcc97cab3014
Oznur Sayin <oznurozturk1@hotmail.com>
2021-03-11 21:41:32 +0400

    Zar projesi eklendi

---
+++ zar/app.js
@@ -0,0 +1,17 @@
+function hadi_zar_at(){
+ 
+
+var RondomOne= Math.ceil(Math.random() * 6);
+var RandomSecond=Math.ceil(Math.random() * 6);
+console.log(RandomSecond);
+
+document.getElementById("birinci_zar").src="img/"+ RondomOne+ ".svg";
+document.getElementById("ikinci_zar").src="img/"+ RandomSecond + ".svg";
+
+}
+
+function birsurebekle(){
+    setTimeout(hadi_zar_at,2000)
+    document.getElementById("birinci_zar").src="img/1.gif";
+document.getElementById("ikinci_zar").src="img/2.gif";
+}
